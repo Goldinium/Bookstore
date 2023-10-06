f98dda5b8d5ac5e32b4f69fb7e0acd14fa775d1b
Badr BenSlama <badrbenslama8@gmail.com>
2023-10-06 15:41:00 +0100

    API fetching data

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addBook, removeBook } from '../redux/books/bookSlice';
+import axios from 'axios';
 
 
 
